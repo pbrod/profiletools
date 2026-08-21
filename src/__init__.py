e41bc2d8134286868eca8fa61332097d5d4101bb
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2026-08-21 02:40:11 +0200

    chore: added profiletools.py

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+from .profiletools import (
+    do_profile,
+    do_cprofile,
+    timefun,
+    TimeWith,
+)
+
+__all__ = [
+    "do_profile",
+    "do_cprofile",
+    "timefun",
+    "TimeWith",
+]
